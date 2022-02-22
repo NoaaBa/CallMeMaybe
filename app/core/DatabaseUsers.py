d54c25203e50f61c43b89a56d401ea75866bad14
Noa <noa27102@gmail.com>
2022-02-22 18:44:58 +0200

    Updated a function that updates the rooms that the user is in

--- app/core/DatabaseUsers.py
+++ app/core/DatabaseUsers.py
@@ -1,15 +1,20 @@
 from app import Config
 from app.entities.User import User
 import json
+import os
 
 
 def add_user_to_db(username, password):
     if not find_user_in_db(username):
         user = User(username, password, None)
-        with open(Config.Links.USERS_DATA, "r") as file:
-            data = json.load(file)
-            data.append(user)
-
+        if os.path.getsize(Config.Links.USERS_DATA) != 0:
+            data = []
+            with open(Config.Links.USERS_DATA, "r") as file:
+                data.append(json.load(file))
+                data.append(user.__dict__)
+        else:
+            data = user.__dict__
+        print(data)
         with open(Config.Links.USERS_DATA, "w") as file:
             json.dump(data, file)
         file.close()
@@ -33,7 +38,17 @@ def transform_user(username):
         data = json.load(file)
     file.close()
     for user in data:
-        if username in user:
+        if username in user['user_id']:
             new_user = User(user['user_id'], user['password'], user['rooms'])
             return new_user
     return False
+
+
+def update_rooms_to_user(username, room):
+    data = dict()
+    with open(Config.Links.USERS_DATA, "r") as file:
+        data = json.load(file)
+    file.close()
+    for user in data:
+        if username in user['user_id']:
+            user['rooms'] = room
