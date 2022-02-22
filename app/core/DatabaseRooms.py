61bf44988aba35d0be906bce2ac43a472700655f
Noa <noa27102@gmail.com>
2022-02-22 18:44:02 +0200

    Class now saves the rooms into a file of list of rooms

--- app/core/DatabaseRooms.py
+++ app/core/DatabaseRooms.py
@@ -1,18 +1,27 @@
 from app import Config
-from app.entities.Room import Room
-import json
+from app.entities import User
+import pickle
+import os
 
 
 def add_room_to_db(name):
     if not find_room_in_db(name):
-        room = Room(name, 0)
-        with open(Config.Links.ROOMS_DATA, "r") as file:
-            data = json.load(file)
-            data.append(room)
-
-        with open(Config.Links.ROOMS_DATA, "w") as file:
-            json.dump(data, file)
-        file.close()
+        # Checking if file is not empty.
+        if os.path.getsize(Config.Links.ROOMS_DATA) != 0:
+            list=[]
+            file = open(Config.Links.ROOMS_DATA,'r')
+            for line in file:
+                list.append(line.strip())
+            list.append(name)
+            file.close()
+            with open(Config.Links.ROOMS_DATA, "w") as file_writer:
+                  for item in list:
+                         file_writer.write('%s\n' % item)
+            file_writer.close()
+        else:
+            with open(Config.Links.ROOMS_DATA, "w") as file_writer:
+               file_writer.write(name)
+            file_writer.close()
         return True
     return False
 
@@ -30,3 +39,4 @@ def find_room_in_db(name):
 def get_list_of_rooms():
     with open(Config.Links.ROOMS_DATA, "r") as file:
         return file.readlines()
+
